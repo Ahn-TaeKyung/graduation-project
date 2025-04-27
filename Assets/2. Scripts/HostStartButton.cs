4c54049351affa04efd0d2d5d1830dda0cdd4c6d
Ahn-TaeKyung <lovetk102@suwon.ac.kr>
2025-04-27 17:10:47 +0900

    모스코시다케

--- Assets/2. Scripts/HostStartButton.cs
+++ Assets/2. Scripts/HostStartButton.cs
@@ -30,17 +30,19 @@ public class HostStartButton : NetworkBehaviour
 
         // (선택) 플레이어들이 역할 선택했는지 확인할 수도 있음
         CheckAllPlayersRole();
-        
-        // 씬 이동
-        if (Runner.SceneManager != null)
+        if(Runner.IsServer)
         {
-            Debug.Log($"SceneChange{m_game_scene_ref}/ {Runner.SceneManager}");   
-            Runner.SceneManager.LoadScene(m_game_scene_ref, new NetworkLoadSceneParameters());
-            Runner.SceneManager.UnloadScene(m_current_scene_ref);
-        }
-        else
-        {
-            Debug.LogError("SceneManager가 없습니다! 씬 이동 실패");
+            // 씬 이동
+            if (Runner.SceneManager != null)
+            {
+                Debug.Log($"SceneChange{m_game_scene_ref}/ {Runner.SceneManager}");   
+                RPC_LoadGameScene();
+                RPC_UnLoadGameScene();
+            }
+            else
+            {
+                Debug.LogError("SceneManager가 없습니다! 씬 이동 실패");
+            }
         }
     }
 
@@ -52,5 +54,21 @@ public class HostStartButton : NetworkBehaviour
         {
             Debug.Log($"플레이어 {role.Object.InputAuthority.PlayerId}의 선택된 역할은 {role.m_player_role}입니다.");
         }
+    }
+
+        // RPC: 모든 클라이언트에게 씬을 로드하라는 명령을 전달
+    [Rpc(RpcSources.InputAuthority, RpcTargets.All)]
+    private void RPC_LoadGameScene()
+    {
+        // 서버에서 씬을 로드한 후 클라이언트들에 씬 로드 요청
+        Runner.SceneManager.LoadScene(m_game_scene_ref, new NetworkLoadSceneParameters());
+    }
+
+        // RPC: 모든 클라이언트에게 씬을 로드하라는 명령을 전달
+    [Rpc(RpcSources.InputAuthority, RpcTargets.All)]
+    private void RPC_UnLoadGameScene()
+    {
+        // 서버에서 씬을 로드한 후 클라이언트들에 씬 로드 요청
+       Runner.SceneManager.UnloadScene(m_current_scene_ref);
     }
 }
